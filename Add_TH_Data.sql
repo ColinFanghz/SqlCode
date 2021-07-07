561a50fb13f5bed1d064a8a8b0afd959da58ec82
Colin <Fanghz_Colin@163.com>
2021-07-07 10:46:13 +0800

    Third Commit

--- Add_TH_Data.sql
+++ Add_TH_Data.sql
@@ -1,1 +1,1 @@
-load data infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/THData.csv' into table lhd fields terminated by ',' optionally enclosed by '"' escaped by '"' lines terminated by '\r\n';
+load data infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/THData.csv' into table th fields terminated by ',' optionally enclosed by '"' escaped by '"' lines terminated by '\r\n';
